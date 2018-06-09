3dce9fce78174c54b03f58b0129348dcc4ecdb45
Javier Martinez Lizama <jfmarli@gmail.com>
2018-06-09 11:35:18 +0200

    added form element to top of fields and now we use the angular default form operations

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
 import { Post } from '../post.model';
 
 @Component({
@@ -12,10 +14,14 @@ export class PostCreateComponent {
   // create a new event which can be listened to from the outside (direct parent)
   @Output() postCreated = new EventEmitter<Post>();
 
-  onAddPost() {
+  onAddPost(form: NgForm) {
+    if(form.invalid){
+      // no post added
+      return;
+    }
     const post: Post = {
-      title: this.enteredTitle,
-      content: this.enteredContent
+      title: form.value.title,
+      content: form.value.content
     };
     // emit the newly created post
     this.postCreated.emit(post)
